464a10e0953583619c5075c81ac819d068ea4bec
IShaLinI <andrewccard04@gmail.com>
2025-02-01 13:04:05 -0800

    Swerve Tuned! for now

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "6d7529317ffae8ea85198ff2b5ef8cb62d2f39db";
-  public static final String GIT_DATE = "2025-01-21 19:31:11 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "57465c81cd97ec61a41d25a59224b23e941d6a0c";
+  public static final String GIT_DATE = "2025-01-27 22:35:49 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-27 18:03:57 EST";
-  public static final long BUILD_UNIX_TIME = 1738019037189L;
+  public static final String BUILD_DATE = "2025-02-01 14:32:16 EST";
+  public static final long BUILD_UNIX_TIME = 1738438336987L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
