10f7733430c6b4eab7fca188a08cea4129a68a6f
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-02-05 17:15:09 -0800

    EOD 2/5 Sim changes

--- src/main/java/frc/robot/subsystems/climber/ClimberIO_Sim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO_Sim.java
@@ -1,3 +1,23 @@
 package frc.robot.subsystems.climber;
 
-public class ClimberIO_Sim implements ClimberIO {}
+import org.littletonrobotics.junction.AutoLogOutput;
+
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.Constants;
+
+public class ClimberIO_Sim implements ClimberIO {
+    // voltage tracking
+    private double voltage = 0;
+
+    // setpoint tracking
+    @AutoLogOutput(key = "Climber/Speed Setpoint")
+    private double speedSetpoint = 0;
+
+    @AutoLogOutput(key = "Climber/Angle Setpoint")
+    private double angleSetpoint = Constants.Climber.minAngle;
+
+    private DCMotorSim climberSim =
+        new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(speedSetpoint, angleSetpoint), null, null)
+}
