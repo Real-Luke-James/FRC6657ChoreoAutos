7d850fe9b4f202b624a6734e1d8ccdd72e8bdf66
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-02-05 16:37:02 -0800

    I have been using Crescendo code and not Reefscape Intake branch

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -5,7 +5,20 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ClimberIO {
 
   @AutoLog
-  public static class ClimberIOInputs {}
+  public static class ClimberIOInputs {
+    public double position = 0.0; // Degrees
+    public double velocity = 0.0; // Degrees per second
+    public double acceleration = 0.0;
+    public double temp = 0.0; // Celcius
+    public double voltage = 0.0; // Volts
+    public double current = 0.0; // Amps
+    public double setpoint = 0.0;
+    public boolean atSetpoint = false;
+
+    public double encoderAbsPosition = 0.0; // Rad
+    public double encoderRelPosition = 0.0; // Rad
+    public double encoderVelocity = 0.0; // Rad/s
+  }
 
   public default void updateInputs(ClimberIOInputs inputs) {}
 
