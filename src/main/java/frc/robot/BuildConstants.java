57fac67032cbb94e9452d442b4add9f5ca8ae7ed
Andrew-6657 <andrew.frankovic@uphsconnect.org>
2025-02-07 10:04:32 -0800

    reverting changes since me and choreo gutted the old project

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "247e3d7c3dcdff98cee97f9bcf899f76a6dd09a7";
-  public static final String GIT_DATE = "2025-02-07 01:48:31 EST";
+  public static final int GIT_REVISION = 86;
+  public static final String GIT_SHA = "85c9d17eb9ff59d47be404e99cbd55aae6f9dd86";
+  public static final String GIT_DATE = "2025-02-07 12:36:24 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-07 12:36:01 EST";
-  public static final long BUILD_UNIX_TIME = 1738949761007L;
+  public static final String BUILD_DATE = "2025-02-07 13:04:01 EST";
+  public static final long BUILD_UNIX_TIME = 1738951441983L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
