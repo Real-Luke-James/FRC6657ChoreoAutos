44cf1287ae359c95edf2a776e144359103c19806
Andrew Card <andrewccard04@gmail.com>
2025-02-06 18:35:56 -0800

    ran spotless

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 84;
-  public static final String GIT_SHA = "709128fa6ec6e75afd92febd2839f13216fd7116";
-  public static final String GIT_DATE = "2025-02-06 21:03:47 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-07 01:45:12 EST";
-  public static final long BUILD_UNIX_TIME = 1738910712157L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 40;
+  public static final String GIT_SHA = "59f5c7d942d4b920e9397af736321b1d73edbf1f";
+  public static final String GIT_DATE = "2025-02-02 00:53:01 EST";
+  public static final String GIT_BRANCH = "Intake";
+  public static final String BUILD_DATE = "2025-02-06 21:31:51 EST";
+  public static final long BUILD_UNIX_TIME = 1738895511081L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
 
