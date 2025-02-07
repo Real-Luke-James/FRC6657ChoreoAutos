d8f639e92a1f7487c07055544e84481c308a8999
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-02-06 18:58:30 -0800

    finish sim code

--- src/main/java/frc/robot/subsystems/climber/ClimberIO_Sim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO_Sim.java
@@ -2,7 +2,11 @@ package frc.robot.subsystems.climber;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.robot.Constants;
 
@@ -19,5 +23,50 @@ public class ClimberIO_Sim implements ClimberIO {
 
     private DCMotorSim climberSim =
         new DCMotorSim(
-            LinearSystemId.createDCMotorSystem(speedSetpoint, angleSetpoint), null, null)
+            LinearSystemId.createDCMotorSystem(DCMotor.getNEO(1), 0.0001, Constants.Climber.gearing),
+            DCMotor.getNEO(1));
+
+    private PIDController climberPID = new PIDController(96d / 360, 0, 0);
+
+    public ClimberIO_Sim() {
+        climberSim.setState(Units.degreesToRadians(Constants.Climber.maxAngle), 0);
+        climberPID.setTolerance(2);
+    }
+
+    @Override
+    public void updateInputs(ClimberIOInputs inputs) {
+
+        updatePID(); // update PID Controller
+
+        // update sim
+        climberSim.update(1 / Constants.mainLoopFrequency);
+
+        // inputs
+        inputs.position = climberSim.getAngularPositionRotations() * 360; // degrees
+        inputs.velocity = climberSim.getAngularVelocityRPM() * (360d / 60); // degrees
+        inputs.temp = 0; // celcius
+        inputs.voltage = voltage; // volts
+        inputs.current = climberSim.getCurrentDrawAmps(); // amps
+        inputs.atSetpoint = climberPID.atSetpoint();
+        inputs.setpoint = angleSetpoint;
+    }
+
+    /**
+   * Change the setpoint of the shooter pivot
+   * 
+   * @param angleDegrees The new setpoint in degrees
+   *  <p>Acceptable Range: Find it. 
+   */
+
+   @Override
+   public void changeSetpoint(double angleDegrees) {
+     angleSetpoint = angleDegrees;
+   }
+
+   private void updatePID() {
+    double climberPIDEffort =
+        climberPID.calculate(climberSim.getAngularPositionRotations() * 360, angleSetpoint);
+    voltage = MathUtil.clamp(climberPIDEffort, -12, 12);
+    climberSim.setInput(voltage);
+   }
 }
